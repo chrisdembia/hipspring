3aef084f5ffe4e98c3df23e36396bba0193d73b3
Christopher Dembia <cld72@cornell.edu>
2012-12-05 16:51:22 -0800

    Added basics.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name='hipspring',
+        author='Chris Dembia',
+        author_email='chris530d@gmail.com',
+        version='0.1',
+        packages=['hipspring'],
+        url='http://github.com/fitze/hipspring',
+        license='LICENSE.txt',
+        long_description=open('README.md').read(),
+        }
